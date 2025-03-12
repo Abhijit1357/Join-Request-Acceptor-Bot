b5ab1b7b57d4d0707234a7c5812d3cdcc0f7e1f2
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-03-12 14:53:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -55,12 +55,23 @@ def schedule_accept_requests():
 
 #Command handlers
 @Client.on_message(filters.command('start'))
-async def start_message(c,m):
-    if not await db.is_user_exist(m.from_user.id):
-        await db.add_user(m.from_user.id, m.from_user.first_name)
-        await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
-        await m.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg", caption=f"<b>Hello {m.from_user.mention} 👋\n\nI Am Join Request Acceptor Bot. I Can Accept All Old Pending Join Request.\n\nFor All Pending Join Request Use - /accept</b>", reply_markup=InlineKeyboardMarkup( [[ InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Tech_VJ') ],[ InlineKeyboardButton("❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/Kingvj01'), InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url='https://t.me/VJ_Botz') ]] ) )
-
+async def start_message(client, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
+    await client.send_message(LOG_CHANNEL, LOG_TEXT.format(message.from_user.id, message.from_user.mention))
+    await message.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg", 
+                                caption=f"<b>Hello {message.from_user.mention} 👋\n\nI Am Join Request Acceptor Bot. I Can Accept All Old Pending Join Request.\n\nFor All Pending Join Request Use - /accept</b>", 
+                                reply_markup=InlineKeyboardMarkup([
+                                    [
+                                        InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Tech_VJ')
+                                    ],
+                                    [
+                                        InlineKeyboardButton("❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/Kingvj01'), 
+                                        InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url='https://t.me/VJ_Botz')
+                                    ]
+                                ])
+                             )
+    
 @Client.on_message(filters.command('accept') & filters.private)
 async def accept(client, message):
     show = await message.reply("**Please Wait.....**")
