95c93023bb4777e4f65d533ae731efc8ac5cb18f
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-03-12 10:06:41 +0530

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -2,11 +2,11 @@ import motor.motor_asyncio
 from config import DB_NAME, DB_URI
 
 class Database:
-    
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
+        self.settings = self.db.settings
 
     def new_user(self, id, name):
         return dict(
@@ -14,15 +14,15 @@ class Database:
             name = name,
             session = None,
         )
-    
+
     async def add_user(self, id, name):
         user = self.new_user(id, name)
         await self.col.insert_one(user)
-    
+
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id':int(id)})
         return bool(user)
-    
+
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
@@ -39,5 +39,12 @@ class Database:
     async def get_session(self, id):
         user = await self.col.find_one({'id': int(id)})
         return user['session']
+
+    async def set_time(self, time):
+        await self.settings.update_one({}, {'$set': {'time': time}}, upsert=True)
+
+    async def get_time(self):
+        settings = await self.settings.find_one({})
+        return settings['time']
 
 db = Database(DB_URI, DB_NAME)
